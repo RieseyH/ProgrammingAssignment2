5ca51b8128bf423a0b395a314fd7783672240f41
Riese.H <118937451+RieseyH@users.noreply.github.com>
2023-01-26 18:31:18 -0800

    this is the week 3 programming assignment where i created a makeVector and a cachemean

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+##first i crated a special vector that set the value 
+##of and got the value of the vector and set the
+##value and got the value of the mean then the cachemean function 
+##calculates the the mean of the daqta and sets the value of the mean
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+##the makeVector creates a special vecor which sets
+##and gets the value og the vector and mean
 
+makeCacheMatrix <- function(x = matrix()){
+  r <- NULL
+  set <- function(y){
+    x <<- y
+    r <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) r <<- inverse
+  getInverse <- function()r
+  list(set = set, get = get, 
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
+## the cacheSolve calculates the mean 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  ## Return a matrix that is the inverse of 'x'
+  r <- x$getInverse()
+  if(!is.null(r)){
+    message("getting cached data")
+    return(r)
+  }
+  mat <- x$get()
+  j <- solve(matt,...)
+  x$setInverse(r)
+  r
 }
